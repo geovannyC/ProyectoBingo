3fd1049f093198662a5f23d88ea4c7b1fed32390
johao70 <perlazajohao@gmail.com>
2020-01-07 22:34:23 -0500

    server integration - eslint and prettier

--- servidor/rutas/routes_db.js
+++ servidor/rutas/routes_db.js
@@ -1,11 +1,11 @@
-const express = require('express')
+const express = require("express");
 let api = express.Router(),
-  control = require('../controles/crud')
+  control = require("../controles/crud");
 
-api.get('/bingo', control.getDatos)
-api.post('/bingo', control.postDatos)
-api.put('/bingo', control.updateDatos )
-api.delete('/bingo', control.deleteDatos)
-api.get('/bingobyid', control.getDatosbyID)
+api.get("/bingo", control.getDatos);
+api.post("/bingo", control.postDatos);
+api.put("/bingo", control.updateDatos);
+api.delete("/bingo", control.deleteDatos);
+api.get("/bingobyid", control.getDatosbyID);
 
-module.exports = api
+module.exports = api;
