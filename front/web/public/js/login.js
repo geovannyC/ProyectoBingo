97b21c8eec2a5ad7ee3e05732e2187dc3caaf48b
johao70 <perlazajohao@gmail.com>
2020-01-08 17:25:22 -0500

    login OK - movil views start

--- front/web/public/js/login.js
+++ front/web/public/js/login.js
@@ -3,45 +3,36 @@ const http = new XMLHttpRequest();
 const API_URL = "http://localhost:8001/server/bingo";
 
 getDataLogin = () => {
+  let correo = document.getElementById("persona_email").value;
+  let contraseña = document.getElementById("persona_clave").value;
+
   http.open("GET", API_URL + "?tabla=persona");
 
   http.responseType = "json";
 
   http.onload = () => {
     let datos = http.response.datos;
-
-    let correo = document.getElementById("persona_email").value;
-    let contraseña = document.getElementById("persona_clave").value;
-
-    console.log(correo);
+    let correoBD;
+    let contraseñaBD;
 
     datos.forEach(element => {
-      if (element.persona_email == correo) {
-        console.log(element.persona_email);
-      } else {
-        return false;
+      if (
+        element.persona_email == correo &&
+        element.persona_clave == contraseña
+      ) {
+        correoBD = element.persona_email;
+        contraseñaBD = element.persona_clave;
       }
     });
-    // for (let i = 0; i<=datos.length; i++){
-    //     let correoBD = (http.response).datos[i].persona_email
-    //     console.log(correoBD);
-    // }
-    //let contraseñaBD = http.response.datos[0].persona_clave;
 
-    //   console.log(contraseñaBD);
-    //   console.log(correo);
-    //   console.log(contraseña);
-
-    // if( correo === correoBD ){
-    //     if( contraseña === contraseñaBD ){
-    //         alert(correo+contraseña)
-    //     }else{
-    //         alert("Contraseña incorrecta")
-    //     }
-    // }else{
-    //     alert("Datos incorrectos, intentelo nuevamente")
-    // }
-    //   document.getElementById('test').innerHTML = usuarioBD;
+    if (correo === correoBD) {
+      if (contraseña === contraseñaBD) {
+        alert(correo + contraseña);
+        window.location.assign("./index.html")
+      } 
+    } else {
+      alert("Datos incorrectos, intentelo nuevamente");
+    }
   };
 
   http.send();
